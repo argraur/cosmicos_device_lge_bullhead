281afe14d14e2700aab21a66f769fe20e8d82a02
argraur <argraur@gmail.com>
2017-07-29 08:17:47 +0000

    aosp_bullhead: Use AEX vendor blobs
    Something changed in my mind and now I want to use AEX blobs.

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -34,8 +34,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 #PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
 
 $(call inherit-product, device/lge/bullhead/device.mk)
-$(call inherit-product, vendor/lge/bullhead/device-system.mk)
-$(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
+$(call inherit-product, vendor/lge/bullhead/bullhead-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/bullhead/bullhead-vendorimage.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3
